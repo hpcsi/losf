8266f37536407cc3fe8ddd6a66a9617df2e6d1d7
Karl W. Schulz <losf@koomie.com>
2011-04-29 18:33:53 +0000

    cleanup old kruft

--- update.sh
+++ update.sh
@@ -16,10 +16,6 @@
 # to define desired RPM packages for each of your node definitions.  A
 # template file is provided in update.template.
 #
-# Ranger Version:   Originally 6/21/07 - ks 
-# Longhorn Version:   10-25-09
-# Lonestar42 Updates: 11-24-10
-#
 # Questions/Problems?: karl@tacc.utexas.edu
 # Texas Advanced Computing Center 
 #----------------------------------------------------------------------
@@ -35,37 +31,11 @@ export UPDATE_RPMS=1
 # End Inputs -------------------
 
 export TOP_DIR=`echo $( (cd -P $(dirname $0) && pwd) )`
-
 export MYHOST=`hostname -s`
-export RPM_DIR=`$TOP_DIR/rpm_topdir grep RPM_TOPDIR | awk '{print $3}'`
-export REMOTE_INSTALL_DIR=/home/build/admin/hpc_stack/
-
-#-------------------------------------
-# Command-Line Options 
-# Used to setup rpm install location
-# for use with PXE initiated installs
-#-------------------------------------
-
-NUM_ARGS=$#
-if [ $# -gt 1 -a "$1" = "PXE" ];then
-    export SRC_DIR=$2
-    export INSTALL_DIR=/home/build/admin/hpc_stack
-    export MODE="ROCKS"
-
-    echo " "
-    echo "** Running update.sh in PXE install mode"
-    echo "** -> Installing from $SRC_DIR"
-else
-    export SRC_DIR=$RPM_DIR
-    export INSTALL_DIR=$REMOTE_INSTALL_DIR
-    export MODE="INTERACTIVE"
-fi
 
 #-------------------
 # Query node type
 #-------------------
-
-
 
 export NODE_TYPE_SILENT=1
 
