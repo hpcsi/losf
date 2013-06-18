b42404c4864a5c741bd0b74c28b49ceaf5831930
Karl W. Schulz <losf@koomie.com>
2013-06-18 21:44:21 +0000

    added status flag

--- utils/history_utils.pl
+++ utils/history_utils.pl
@@ -30,8 +30,13 @@ use warnings;
 
 my %node_history        = ();
 my $DATA_VERSION        = "1.0";
-my $HOST_ENTRY_SIZE_1_0 = 4;
+my $HOST_ENTRY_SIZE_1_0 = 5;
 my $DATA_FILE="/admin/build/admin/hpc_stack/.losf_log_data";
+
+use constant {
+    CLOSE_ERROR   => 1,
+    CLOSE_NOERROR => 2,
+};
 
 sub add_node_event
 {
@@ -39,13 +44,16 @@ sub add_node_event
     my $action     = shift;
     my $comment    = shift;
     my $admin_user = shift;
+    my $flag       = shift;
 
     my $timestamp;
 
     my $remain_args = @_;
     if ( $remain_args == 1) {
 	$timestamp = shift;
+
 	# validate timestamp (e.g. 2013-06-18 15:50)
+
 	if ($timestamp !~ m/\d\d\d\d-\d\d-\d\d \d\d:\d\d/) {
 	    print "ERROR: malformed user-provided timestamp: expect 24 hour date of the form -> 2013-06-18 15:50\n";
 	    exit 1;
@@ -62,7 +70,14 @@ sub add_node_event
 				   $action ne "open"    && 
 				   $action ne "comment");
 
-    push @{$node_history{$host} },($timestamp,$action,$comment,$admin_user);
+    # validate flag
+    
+    if($flag != CLOSE_ERROR && $flag != CLOSE_NOERROR) {
+	print "ERROR: unknown host close flag provided\n";
+	exit 1;
+    }
+
+    push @{$node_history{$host} },($timestamp,$action,$comment,$admin_user,$flag);
 
 }
 
@@ -75,22 +90,8 @@ sub save_state_1_0
 
 sub read_state_1_0
 {
-####    # Initiate shared file lock for reading
-###
-####    open (FH,"< $DATA_FILE") or die "Unable to open $DATA_FILE: $!";
-####    flock(FH, LOCK_SH)       or die "Cannot lock $DATA_FILE: $!";
-###
-###    # Read the state
-###
-####    ($DATA_VERSION,%node_history) = @{retrieve(*FH)};
-###
-###    # Release the lock
-###
-####    close(FH);
-###
-####    ($DATA_VERSION,%node_history) = @{retrieve ($DATA_FILE)};
-
     # use locking retrieve to read latest state 
+
     ($DATA_VERSION,%node_history) = @{lock_retrieve ($DATA_FILE)};
 }
 
@@ -100,8 +101,9 @@ sub dump_state_1_0
     print "-" x 94;
     print "--- DATA VERSION = $DATA_VERSION ---";
     print "\n";
-    print "Hostname         Timestamp         Action           Comment                      ";
-    print "                                 User\n";
+    print "Hostname      Timestamp     E Action  Comment";
+    print " " x 71;
+    print "User\n";
     for my $key (keys %node_history) {
 	my $num_entries = @{$node_history{$key}};
 	
@@ -109,10 +111,17 @@ sub dump_state_1_0
 	my $count =0;
 	for($count=0;$count<$num_entries;$count+=$HOST_ENTRY_SIZE_1_0) {
 	    printf("%-10s ", $key);
-	    printf("%-22s ",($value[$count+0]));
-	    printf("%7s   ",($value[$count+1]));
-	    printf("%-66s ",($value[$count+2]));
-	    printf("%8s ",  ($value[$count+3]));
+	    my $flag="";
+	    if($value[$count+4] eq CLOSE_ERROR) {
+		$flag="X";
+	    }
+
+
+	    printf("%-16s ",($value[$count+0]));  # timestamp
+	    printf("%1s ",$flag);                       # error flag
+	    printf("%6s ",($value[$count+1]));         # state
+	    printf(" %-73s ",($value[$count+2]));        # comment
+	    printf("%8s",  ($value[$count+3]));        # user
 	    printf("\n");
 	}
 
@@ -129,9 +138,9 @@ sub clear_state
 
 my %node_status  = ("c401-101" => 0, "c401-102" => 1, "c401-103" => 0);
 
-add_node_event("c401-101","open","just a test","koomie","2013-01-07 08:30");
-add_node_event("c401-101","close","uh oh","koomie");
-add_node_event("c401-102","open","just a test2","koomie");
+add_node_event("c401-101","open","just a test","koomie",1,"2013-01-07 08:30");
+add_node_event("c401-101","close","uh oh","koomie",2);
+add_node_event("c401-102","open","just a test2","koomie",1);
 
 save_state_1_0();
 clear_state();
