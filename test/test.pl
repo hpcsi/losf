cd58ad4c3a1f6c140a61826536911be12742ad8d
Karl W. Schulz <karl@koomie.com>
2014-04-25 15:40:16 -0500

    rpm_topdir tests

--- test/test.pl
+++ test/test.pl
@@ -59,6 +59,20 @@ $igot =~ s/\e\[\d+m//g;		# remove any colors
 my $expect = "OK: [RPMs: OS 0/0  Custom 0/0] [Files: 0/0] [Links: 0/0] [Services: 0/0] [Perms: 0/0] -> master";
 
 ok("$igot" eq "$expect","update output ok");
+ok(system("$losf_dir/rpm_topdir -q 1> $tmpdir/.result" ) == 0,"rpm_topdir runs");
+
+open(IN,"<$tmpdir/.result")     || die "Cannot open $tmpdir/.result\n";
+
+$line = <IN>; chomp($line);
+ok($line =~ m/^\[LosF\] Node type:       test -> master$/,"rpm_topdir -> correct node type");
+
+$line = <IN>; chomp($line);
+ok($line =~ m/^\[LosF\] Config dir:      $tmpdir$/,"rpm_topdir -> correct config dir");
+
+$line = <IN>; chomp($line);
+ok($line =~ m/^\[LosF\] RPM topdir:      $tmpdir\/test\/rpms$/,"rpm_topdir -> correct RPM topdir ");
+
+close(IN);
 
 done_testing();
 
