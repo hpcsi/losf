7855ef4842463e906c9bccd6921598c54005ec5a
Karl W. Schulz <karl@koomie.com>
2014-05-04 11:23:26 -0500

    Use module name for test_utils

--- test/test_utils.pm
+++ test/test_utils.pm
@@ -1,1 +1,10 @@
-test_utils.pl
+#!/usr/bin/env perl
+
+sub test_binary_existence {
+    my $binary = shift;
+    my $basename = basename($binary);
+    ok (-x "$binary", "$basename binary");
+    return;
+}
+
+1;
