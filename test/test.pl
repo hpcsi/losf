958add709a042d44660c84a790cf7c7107a01f16
Karl W. Schulz <karl.w.schulz@intel.com>
2015-03-12 14:45:43 -0700

    add new tests for versioning consistency; update check for update output to reflect additional info in print.

--- test/test.pl
+++ test/test.pl
@@ -27,7 +27,7 @@
 use strict;
 
 use Test::More;
-use Test::More tests => 21;
+use Test::More tests => 27;
 #use Test::Files;
 use File::Basename;
 use File::Temp qw(tempfile);
@@ -52,6 +52,28 @@ my @BINS=("losf","update","node_types","koomie_cf",
 foreach my $bin (@BINS) {
     test_binary_existence("$losf_dir/$bin");
 }
+
+#------------------------------------------------------
+print "\nChecking versioning consistency:\n";
+my $config_ac="$losf_dir/configure.ac";
+
+ok(-s "$losf_dir/VERSION","version file present");
+ok(-s "$losf_dir/configure.ac","configure.ac file present");
+
+my $loc_version=`cat $losf_dir/VERSION`; chomp($loc_version);
+
+open(IN,"$losf_dir/configure.ac") || die "Cannot open configure.ac\n";
+ok ( (grep{/\[$loc_version\]/} <IN>) ,"version file matches configure.ac");
+close(IN);
+
+my $version_update=`$losf_dir/update -v | grep Version`;
+ok ($version_update == "LosF: Version $loc_version","\"update -v\" matches manifest");
+
+my $version_losf=`$losf_dir/losf -v | grep Version`;
+ok ($version_update == "LosF: Version $loc_version","\"losf -v\" matches manifest");
+
+my $version_losf=`$losf_dir/losf --version | grep Version`;
+ok ($version_update == "LosF: Version $loc_version","\"losf --version\" matches manifest");
 
 #------------------------------------------------------
 
@@ -81,7 +103,7 @@ my $igot=(`cat $tmpdir/.result`);
 chomp($igot);			# remove newline
 $igot =~ s/\e\[\d+m//g;		# remove any colors
 
-my $expect = "OK: [RPMs: OS 0/0  Custom 0/0] [Files: 0/0] [Links: 0/0] [Services: 0/0] [Perms: 0/0] -> master";
+my $expect = "OK: [RPMs: OS     0/0  Custom     0/0] [Files:    0/0] [Links:   0/0] [Services:   0/0] [Perms:   0/0] -> master";
 
 ok("$igot" eq "$expect","update output ok");
 ok(system("$losf_dir/rpm_topdir -q 1> $tmpdir/.result" ) == 0,"rpm_topdir runs");
